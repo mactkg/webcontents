f6f53f1cd361bde49e45c1e9bd9a3e5972a6f04f
mactkg <mactkg@gmail.com>
2015-07-23 17:49:08 +0900

    devide script

--- bin/import.js
+++ bin/import.js
@@ -1,25 +1,13 @@
-var fs   = require('fs'),
-    path = require('path'),
-    exec = require('child_process').exec;
-    execSync = require('child_process').execSync;
-
-var importJSON = function(file_path, db_name, col_name) {
-  file_path = path.resolve(file_path);
-  var cmd = "mongoimport";
-      cmd += " --type json --jsonArray";
-      cmd += " --db " + db_name;
-      cmd += " --collection " + col_name;
-      cmd += " --file \"" + file_path + '"';
-  console.log(cmd);
-  console.log(execSync(cmd));
-}
+var fs       = require('fs'),
+    path     = require('path'),
+    db       = require('../lib/db.js');
 
 console.log(process.env.DATA_DIR || process.argv[2] || '/Users/kenta/Downloads/keitalab Slack export Jul 22 2015');
 DATA_DIR = path.resolve(process.env.DATA_DIR || process.argv[2] || '/Users/kenta/Downloads/keitalab Slack export Jul 22 2015');
 DB_NAME = process.env.DB_NAME || 'slack';
 
 // import user
-importJSON(DATA_DIR+"/users.json", DB_NAME, "users");
+console.log(db.importJSON(DATA_DIR+"/users.json", DB_NAME, "users").toString());
 
 // get channel
 channels = JSON.parse(fs.readFileSync(DATA_DIR+"/channels.json"));
@@ -30,6 +18,6 @@ channels.forEach(function(e) {
   var files = fs.readdirSync(channel_path);
   files.forEach(function(f) {
     console.log(channel_path+"/"+f);
-    importJSON(channel_path+"/"+f, DB_NAME, e["name"]);
+    console.log(db.importJSON(channel_path+"/"+f, DB_NAME, e["name"]).toString());
   });
 });
