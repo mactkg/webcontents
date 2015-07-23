3874e27f3bf182019acd18ef14e38162dbd269fb
mactkg <mactkg@gmail.com>
2015-07-24 04:30:09 +0900

    add some public api for visualize

---
+++ routes/api.js
@@ -0,0 +1,31 @@
+var express = require('express'),
+    stats   = require('../lib/stats');
+var router = express.Router();
+
+router.get('/channels', function(req, res, next) {
+  stats.getChannels().then(function(channels) {
+    res.json(channels);
+  });
+});
+
+router.get('/channels/:name/stats', function(req, res, next) {
+  stats.getChannelStats(req.params.name, req.query.style)
+    .then(function(data) {
+      res.json(data);
+    });
+});
+
+router.get('/channels/stats', function(req, res, next) {
+  stats.getAllChannelStats(req.query.style).then(function(data) {
+    res.json(data);
+  });
+
+});
+
+router.get('/users', function(req, res, next) {
+  stats.getUsers().then(function(users) {
+      res.json(users);
+    });
+});
+
+module.exports = router;
